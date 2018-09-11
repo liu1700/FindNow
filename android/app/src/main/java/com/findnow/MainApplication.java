63751955ee3d4ef12ccc9ff2f72669007f1d3d07
liu1700 <liu170045@gmail.com>
2018-09-11 22:27:59 +0800

    Revert "remove google maps"
    This reverts commit 8dc59d2cfaf6d6176c7f6ac2cff59e1da702c57e.

--- android/app/src/main/java/com/findnow/MainApplication.java
+++ android/app/src/main/java/com/findnow/MainApplication.java
@@ -10,6 +10,8 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
+import com.airbnb.android.react.maps.MapsPackage;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -26,7 +28,8 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new RNDeviceInfo(),
-          new VectorIconsPackage()
+          new VectorIconsPackage(),
+          new MapsPackage()
       );
     }
 
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -9,6 +9,7 @@ import {
   Text,
   TextInput,
 } from 'react-native';
+import MapView from 'react-native-maps';
 
 import { MonoText, TabBarTitleText } from '../components/StyledText';
 import { Toolbar } from 'react-native-material-ui';
@@ -47,6 +48,13 @@ export default class HomeScreen extends React.Component {
   render() {
     return (
       <KeyboardAvoidingView style={styles.container}>
+        <MapView style={styles.mapContainer}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }} ></MapView>
 
         <MonoText
           style={{ position: "absolute", top: 10 }}
