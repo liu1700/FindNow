cc7741abe985b745c02edee7f0713a80a652b99f
liu1700 <liu170045@gmail.com>
2018-09-11 23:42:29 +0800

    add google sign in

--- android/app/src/main/java/com/findnow/MainApplication.java
+++ android/app/src/main/java/com/findnow/MainApplication.java
@@ -3,6 +3,7 @@ package com.findnow;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import co.apptailor.googlesignin.RNGoogleSigninPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNGoogleSigninPackage(),
             new RNDeviceInfo(),
           new VectorIconsPackage(),
           new MapsPackage()
