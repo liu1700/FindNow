ff3ff73b523f52b747be0fc335d33bb4cba0cfda
YuChen Liu <yuchen.liu@grabtaxi.com>
2018-09-20 09:46:46 +0800

    update collapse

--- components/ChattingCollapse.js
+++ components/ChattingCollapse.js
@@ -1,22 +1,14 @@
 import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
-import { Badge, Button } from 'react-native-elements';
-import Collapsible from 'react-native-collapsible';
+import { ActionButton } from 'react-native-elements';
+import Accordion from 'react-native-collapsible/Accordion';
 
 export default class ChattingCollapse extends React.Component {
-
     render() {
         return (
-            <View style={styles.container}>
-                <Badge style={styles.badge}>
-                    <Text>99</Text>
-                </Badge>
-                <Button style={styles.chatButton}
-                    title={null}
-                    rounded={true}
-                    icon={{ name: 'chat', type: 'entypo' }} >
-                </Button>
-            </View>
+            <Collapsible collapsed={false}>
+                <Text>99</Text>
+            </Collapsible>
         );
     }
 }
