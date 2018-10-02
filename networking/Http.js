4574e5b562f3028647837498f9819e6795d6a964
liu1700 <liu170045@gmail.com>
2018-10-02 18:39:02 +0800

    update req validation

--- networking/Http.js
+++ networking/Http.js
@@ -3,12 +3,17 @@ const baseURL = 'http://192.168.0.102:8087'
 const apiVersion = '/v1'
 const requestURL = baseURL + apiVersion
 
+const version = '0.0.1'
+const name = 'findnow'
+const content = 'techs'
+
 export function BuildPost(reqBody) {
     return {
         method: 'POST',
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json',
+            'FINDNOW-NAME-ID': version + name + content,
         },
         body: JSON.stringify(reqBody),
     }
